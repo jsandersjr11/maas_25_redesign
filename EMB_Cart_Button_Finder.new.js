3e0af28924862cfbe60a5e157319e111a4533bcc
Jeff Sanders <kepi.creative@gmail.com>
2025-11-05 14:40:16 -0700

    testing link update for CTL

--- EMB_Cart_Button_Finder.new.js
+++ EMB_Cart_Button_Finder.new.js
@@ -1009,7 +1009,7 @@
       }
 
       // Build the buyflow URL with the extracted values
-      let buyflowUrl = `https://px-demo-ordering.lumen.com/index?salescode=${salescode}&partnerId=clearlink${
+      let buyflowUrl = `https://px-test-ordering.lumen.com/index?salescode=${salescode}&partnerId=clearlink${
         clearlinkeventid ? `&acsid=${clearlinkeventid}` : ''
       }`;
 
