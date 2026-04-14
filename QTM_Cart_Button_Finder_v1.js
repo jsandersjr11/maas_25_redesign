ee9bfe191980be7010f91719bae2a56874b2633d
Jeff Sanders <kepi.creative@gmail.com>
2026-04-14 09:28:47 -0600

    QTM updated default sales code

--- QTM_Cart_Button_Finder_v1.js
+++ QTM_Cart_Button_Finder_v1.js
@@ -215,7 +215,7 @@
       };
  
       // Defaults
-      let salescode = '20001139';
+      let salescode = '8009446';
       let clearlinkeventid = '';
       const defaultTn = '1111111111';
       let tn = defaultTn;
